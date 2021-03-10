2eea6eda08822f7819790c5e769fe3328b16dc71
ntuunam <80401480+ntuunam@users.noreply.github.com>
2021-03-10 10:38:47 -0300

    ver interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,39 @@
 from tkinter import *
+from tkinter import ttk
+import mysql.connector
+ventana = Tk()
 
-window = Tk()
+ventana.title("Desafio nivel inicial")
 
-window.title("Welcome to LikeGeeks app")
+titulo = Label(ventana, text="Formulario de Inscripacion")
+docu = Label(ventana, text="Doc. N° ")
+mail = Label(ventana, text="Email ")
+contra = Label(ventana, text="Contraseña ")
+repe_contra = Label(ventana, text="Repetir Contraseña ")
+phone = Label(ventana, text="Telefono ")
+enviar = Button(ventana, text="Enviar")
+salir = Button(ventana, text="Salir")
+docu_entry = Entry(ventana, width=15, justify=CENTER)
+mail_entry = Entry(ventana, width=15, justify=CENTER)
+contra_entry = Entry(ventana, width=15, justify=CENTER)
+repe_contra_entry = Entry(ventana, width=15, justify=CENTER)
+phone_entry = Entry(ventana, width=15, justify=CENTER)
 
-window.mainloop()
+
+titulo.grid(row=0, column=0, columnspan=2)
+docu.grid(row=1, column=0, padx=5, pady=5)
+mail.grid(row=2, column=0, padx=5, pady=5)
+contra.grid(row=3, column=0, padx=5, pady=5)
+repe_contra.grid(row=4, column=0, padx=5, pady=5)
+phone.grid(row=5, column=0, padx=5, pady=5)
+enviar.grid(row=6, column=1, padx=5, pady=5)
+salir.grid(row=6, column=2, padx=5, pady=5)
+docu_entry.grid(row=1, column=1, padx=5, pady=5)
+mail_entry.grid(row=2, column=1, padx=5, pady=5)
+contra_entry.grid(row=3, column=1, padx=5, pady=5)
+repe_contra_entry.grid(row=4, column=1, padx=5, pady=5)
+phone_entry.grid(row=5, column=1, padx=5, pady=5)
+
+
+
+ventana.mainloop()
